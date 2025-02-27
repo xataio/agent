b4028287036098042c8cb6a554ee4db75a48c2ce
Alexis Rico <sferadev@gmail.com>
2025-02-27 10:32:49 +0100

    Add fake auth
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- apps/dbagent/src/auth.ts
+++ apps/dbagent/src/auth.ts
@@ -1,19 +1,35 @@
 import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 import { env } from './lib/env/server';
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    {
-      id: 'openid',
-      name: 'OpenID',
-      type: 'oidc',
-      options: {
-        clientId: env.AUTH_OPENID_ID,
-        clientSecret: env.AUTH_OPENID_SECRET,
-        issuer: env.AUTH_OPENID_ISSUER
+function getProviders() {
+  if (env.AUTH_OPENID_ID && env.AUTH_OPENID_SECRET && env.AUTH_OPENID_ISSUER) {
+    return [
+      {
+        id: 'openid',
+        name: 'OpenID',
+        type: 'oidc',
+        options: {
+          clientId: env.AUTH_OPENID_ID,
+          clientSecret: env.AUTH_OPENID_SECRET,
+          issuer: env.AUTH_OPENID_ISSUER
+        }
+      } as const
+    ];
+  }
+
+  return [
+    Credentials({
+      name: 'Local auth',
+      async authorize() {
+        return { id: '1', name: 'User', email: 'user@example.com' };
       }
-    }
-  ],
+    })
+  ];
+}
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: getProviders(),
   secret: env.AUTH_SECRET,
   session: {
     strategy: 'jwt'
