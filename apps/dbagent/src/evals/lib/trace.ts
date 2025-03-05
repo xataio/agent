2d98591ed7667ebff9a9e00147b35ae3f977f81f
Richard Gill <richard@rgill.co.uk>
2025-03-05 06:06:39 +0000

    human tracing

--- apps/dbagent/src/evals/lib/trace.ts
+++ apps/dbagent/src/evals/lib/trace.ts
@@ -3,13 +3,52 @@ import fs from 'fs';
 import path from 'path';
 import { ensureTraceFolderExists } from './testId';
 
-export const traceVercelAiResponse = (response: Awaited<ReturnType<typeof generateText>>) => {
+type GenerateTextResponse = Awaited<ReturnType<typeof generateText>>;
+
+const toolResultToHuman = (toolResult: any) => {
+  return `
+${toolResult.toolName} with args: ${JSON.stringify(toolResult.args)}
+
+Tool Result: ${toolResult.result}
+`;
+};
+
+export const stepToHuman = (step: GenerateTextResponse['steps'][0]) => {
+  return `
+${step.text}
+${step.toolResults.map(toolResultToHuman).join('\n\n')}
+`;
+};
+
+const parseRequestBody = (response: GenerateTextResponse) => {
+  const requestBody = response.request.body;
+  if (!requestBody) {
+    throw new Error('No request body found in response');
+  }
+  return JSON.parse(requestBody);
+};
+
+const getSystemPromptFromResponse = (response: GenerateTextResponse) => {
+  const body = parseRequestBody(response);
+  return body.system[0].text;
+};
+
+const getUserPromptFromResponse = (response: GenerateTextResponse) => {
+  const body = parseRequestBody(response);
+  return body.messages[0].content[0].text;
+};
+
+export const traceVercelAiResponse = (response: GenerateTextResponse) => {
   const traceFolder = ensureTraceFolderExists();
 
   const humanTraceFile = path.join(traceFolder, 'human.txt');
   const humanTrace = `
-    Text: ${response.text}
-  `;
+System Prompt: ${getSystemPromptFromResponse(response)}
+--------
+User Prompt: ${getUserPromptFromResponse(response)}
+--------
+${response.steps.map((step, index) => `Step: ${index + 1}\n\n${stepToHuman(step)}`).join('--------\n\n')}
+`;
   fs.writeFileSync(humanTraceFile, humanTrace);
 
   const responseJson = path.join(traceFolder, 'response.json');
