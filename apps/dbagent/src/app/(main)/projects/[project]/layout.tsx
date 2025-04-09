b533f77e40cacf1e5f25f5061330d1809742923e
Alexis Rico <sferadev@gmail.com>
2025-04-09 16:55:25 +0200

    Small UI updates
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- apps/dbagent/src/app/(main)/projects/[project]/layout.tsx
+++ apps/dbagent/src/app/(main)/projects/[project]/layout.tsx
@@ -1,6 +1,7 @@
 import { SidebarInset } from '@internal/components';
 import { notFound } from 'next/navigation';
 import { getCompletedTaskPercentage } from '~/components/onboarding/actions';
+import { Container } from '~/components/ui/container';
 import { SideNav } from '~/components/ui/side-nav';
 import { getProject } from './actions';
 
@@ -28,10 +29,8 @@ export default async function Layout({
     <div className="flex h-full w-full">
       <SideNav project={project} onboardingComplete={onboardingComplete} />
 
-      <SidebarInset className="w-full">
-        <div className="main-content mx-auto flex w-full max-w-6xl flex-col">
-          <div className="relative flex-1 px-8 pt-24">{children}</div>
-        </div>
+      <SidebarInset>
+        <Container>{children}</Container>
       </SidebarInset>
     </div>
   );
