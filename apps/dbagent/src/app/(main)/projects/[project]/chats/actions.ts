0e65c9c0b7cfe05a5b476822bce79a8fa6e4df20
Steffen Siering <steffen@xata.io>
2025-05-12 15:47:05 +0200

    Monitoring: NodeSDK + Otel + Langfuse (#168)
    Alternative implementation to #156 
    
    This change uses the NodeSDK and does not rely on @vercel/otel package.
    On the plus side, I haven't had to fight version incompatibilities
    between a few packages.

--- apps/dbagent/src/app/(main)/projects/[project]/chats/actions.ts
+++ apps/dbagent/src/app/(main)/projects/[project]/chats/actions.ts
@@ -8,6 +8,12 @@ import { getUserSessionDBAccess } from '~/lib/db/db';
 export async function generateTitleFromUserMessage({ message }: { message: Message }) {
   const { text: title } = await generateText({
     model: await getModelInstance('title'),
+    experimental_telemetry: {
+      isEnabled: true,
+      metadata: {
+        tags: ['internal', 'chat', 'title']
+      }
+    },
     system: `\n
     - you will generate a short title based on the first message a user begins a conversation with
     - ensure it is not more than 80 characters long
